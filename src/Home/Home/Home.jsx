6efe3c5a09d56fdf802db471f53f4948cc2d7ff3
Hamiduzzaman96 <hamiduzzaman.ramim96@gmail.com>
2023-12-08 11:11:25 +0600

    fourth  commit

--- src/Home/Home/Home.jsx
+++ src/Home/Home/Home.jsx
@@ -1,8 +1,18 @@
+import './Home.css'
+import img1 from '../../assets/ramim-01.png'
+
 const Home = () =>{
     return(
-        <div>
-           
-        </div>
+      <>
+         <div className='full'>
+            <div className='text'>
+                <h1>I am Hamiduzzaman Ramim . </h1>
+            </div>
+            <div className='picture'>
+                <img src={img1} alt="" />
+            </div>
+         </div>
+      </>
     );
 }
 export default Home;
